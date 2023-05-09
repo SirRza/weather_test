1ffab582aedfff6233de7345b80f11e50fa383ef
Rza <re.shamsipour.za@gmail.com>
2023-05-09 14:26:14 +0330

    Comment Swagger

--- test_app/__init__.py
+++ test_app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from test_app.routes.controller import bp
 from test_app.models import db
-from flasgger import Swagger
+#from flasgger import Swagger
 
 
 
 app = Flask(__name__)
-swagger = Swagger(app)
+#swagger = Swagger(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///testData.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
