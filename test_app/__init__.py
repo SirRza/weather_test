6b52dc3b6e76f5c12cc56bf419f860bf2fc12c04
Rza <re.shamsipour.za@gmail.com>
2023-05-10 12:34:14 +0330

    try to fix

--- test_app/__init__.py
+++ test_app/__init__.py
@@ -24,7 +24,7 @@ def handler_error_for_db(error):
 
 #Sqllite Config
 #app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///testData.db"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://{user}:{password}@{server}:{port}/{database}'.format(user='root',password='HackMeIfYouCan@37HahA',server='localhost', database='test_db', port=3307)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:HackMeIfYouCan@37HahA@mysql:3307/test_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
