8bd0540e32b82e920016197401d7eb973ae7a767
Rza <re.shamsipour.za@gmail.com>
2023-05-10 14:55:14 +0330

    try to fix

--- test_app/routes/test_app.py
+++ test_app/routes/test_app.py
@@ -5,7 +5,7 @@ import uuid as uid
 import redis
 
 
-redis_client = redis.Redis('redis', port=6379, db=0)
+redis_client = redis.Redis('redis', port=6380, db=0)
 
 
 bp = Blueprint("route", __name__)
