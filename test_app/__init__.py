32634ea4550bcb21f0637810683bde720e1b547d
Rza <re.shamsipour.za@gmail.com>
2023-05-13 10:05:34 +0330

    fix requirements

--- test_app/__init__.py
+++ test_app/__init__.py
@@ -3,7 +3,6 @@ from test_app.routes.test_app import bp
 from test_app.models.test_app import db
 from redis.exceptions import ConnectionError
 from sqlalchemy import exc as sqlalchemy_exec
-import MySQLdb
 #from flasgger import Swagger
 
 
@@ -24,7 +23,7 @@ def handler_error_for_db(error):
 
 #Sqllite Config
 #app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///testData.db"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456789@127.0.0.1:3306/test_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@127.0.0.1:3306/test_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
